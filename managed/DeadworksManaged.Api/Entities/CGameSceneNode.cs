1d5abc71b679ea8b7b81b39e12a9c8da5bb41b94
perrccyy <188545258+perrccyy@users.noreply.github.com>
2026-03-18 12:44:28 -0400

    expose CGameSceneNode.m_flScale

--- managed/DeadworksManaged.Api/Entities/CGameSceneNode.cs
+++ managed/DeadworksManaged.Api/Entities/CGameSceneNode.cs
@@ -8,4 +8,7 @@ public unsafe class CGameSceneNode : NativeEntity {
 
 	private static readonly SchemaAccessor<Vector3> _vecAbsOrigin = new("CGameSceneNode"u8, "m_vecAbsOrigin"u8);
 	public Vector3 AbsOrigin => _vecAbsOrigin.Get(Handle);
+
+	private static readonly SchemaAccessor<float> _flScale = new("CGameSceneNode"u8, "m_flScale"u8);
+	public float Scale { get => _flScale.Get(Handle); set => _flScale.Set(Handle, value); }
 }
